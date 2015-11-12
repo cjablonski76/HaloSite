cc77decefa2cc20cc66e6c020085a69a667091be
unknown <codyj@codyj-ws1.microsystems.com>
2015-11-12 16:06:54 -0600

    remove auth token from code

--- src/HaloSite/Startup.cs
+++ src/HaloSite/Startup.cs
@@ -2,19 +2,33 @@
 using System.Net.Http;
 using HaloSite.Controllers;
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Hosting;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Configuration;
+using Microsoft.Framework.Runtime;
 
 namespace HaloSite
 {
     public class Startup
     {
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+		public IConfiguration Configuration { get; set; }
+
+		public Startup(IApplicationEnvironment appEnv)
+		{
+			//var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
+			//  .AddJsonFile("config.json")
+			//  .AddUserSecrets()
+			//  .AddEnvironmentVariables();
+
+			//Configuration = builder.Build();
+		}
+
+		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
         {
 	        services.AddTransient<IHaloService, HaloService>();
 	        var httpClient = new HttpClient {BaseAddress = new Uri("https://www.haloapi.com/")};
-			httpClient.DefaultRequestHeaders.Add(ApiTokenHeader, "6b2a9a0cc1454506b3f24538a0a7dc13");
+			httpClient.DefaultRequestHeaders.Add(ApiTokenHeader, "abc123");
 			services.AddSingleton<IHaloHttpClient, HaloHttpClient>(provider => new HaloHttpClient(httpClient));
 	        services.AddMvc();
         }
